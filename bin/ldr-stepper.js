37644c3cb58c4709cbb693f730ea00467eec1c3a
Christopher Hiller <boneskull@boneskull.com>
2015-09-09 12:20:31 -0700

    make programmatic interface a bit nicer, maybe.

--- bin/ldr-stepper.js
+++ bin/ldr-stepper.js
@@ -7,6 +7,16 @@ var generateSteps = require('../lib');
 var path = require('path');
 var omit = require('lodash.omit');
 
+/**
+ * These extensions will not generate a warning.
+ * @type {string[]}
+ */
+var VALID_EXTENSIONS = [
+  '.ldr',
+  '.dat',
+  '.mpd'
+];
+
 var argv = yargs
   .usage('$0 [options] <file.ldr>')
   .demand(1)
@@ -49,6 +59,8 @@ var argv = yargs
   .check(function check(args) {
     var filepath = args._[0];
     var base;
+    var ext;
+
     if (args.steps) {
       args.steps = Math.floor(args.steps);
       if (args.steps < 0) {
@@ -61,7 +73,9 @@ var argv = yargs
       throw new Error('--pieces must be a positive integer');
     }
 
-    if (!generateSteps.checkExtension(filepath)) {
+    ext = path.extname(filepath).toLowerCase();
+
+    if (VALID_EXTENSIONS.indexOf(ext) === -1) {
       base = path.basename(filepath);
       console.warn('WARNING: "%s" may not be an LDR file', base);
     }
